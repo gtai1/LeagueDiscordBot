0112feb9fc22f8bc77851b2bb447594b131abcba
Gavin <tai.gavin@gmail.com>
2023-09-28 10:56:27 -0400

    update to new school js strings

--- PlayerIdFetcher.js
+++ PlayerIdFetcher.js
@@ -11,19 +11,19 @@ for (let player in playerList) {
     const summonerInfo = await playerIdResponse.text();
     const playerId = JSON.parse(summonerInfo).id;
 
-    console.log("[" + playerList[player] + "] playerId = [" + playerId + "]");
+    console.log(`[${playerList[player]}] playerId = [${playerId}]`);
 
     //fetch response code of player fetch to see if in game or not (404 not in game, 200 in game)
     const playerStatusResponse = await fetch(`https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${playerId}?api_key=${APIKey}`);
     const statusCode = playerStatusResponse.status;
 
     if(statusCode == 400) { //bad request
-        console.log("Status code for " + playerList[player] + " is " + statusCode + ". Bad Request.");
+        console.log(`Status code for ${playerList[player]} is ${statusCode}. Bad Request.`)
     }
     else if(statusCode == 200) { //in game
-        console.log("Status code for " + playerList[player] + " is " + statusCode + ". They are currently in game!");
+        console.log(`Status code for ${playerList[player]} is ${statusCode}. They are currently in game!`)
     }
     else { //not in game
-        console.log("Status code for " + playerList[player] + " is " + statusCode + ". They are currently not in game.");
+        console.log(`Status code for ${playerList[player]} is ${statusCode}. They are currently not in game.`)
     }
 }

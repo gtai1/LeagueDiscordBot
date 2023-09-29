85f7ab8ab49d70552c9857d54c76a710d19f3596
Gavin <tai.gavin@gmail.com>
2023-09-29 17:37:39 -0400

    change logic of playerIdfetcher to provide associated discord names and lol names

--- PlayerIdFetcher.js
+++ PlayerIdFetcher.js
@@ -1,15 +1,23 @@
 import fetch from 'node-fetch';
-// import keys from './keys.json' assert { type: "json" };
-import lolPlayers from './playerList.json' assert { type: 'json' };
+import accountsList from './accountsList.json' assert { type: 'json' };
 import 'dotenv/config';
 
 export default async function playerFetcher() {
 	//list of summoner names you want to track (riot limits 20 calls per second, 100 per 2 minutes)
-	const players = lolPlayers.lolPlayerList;
+	const playerAccountInfo = accountsList.accounts;
 	const APIKey = process.env.RIOT_API_KEY;
-	const playersInGame = [];
+    const leagueNameList = [];
+    const playersInGame = [];
 
-	for (let player of players) {
+    //put all the league names in a list
+    for (let account of playerAccountInfo) {
+        for (let leagueName of account.leagueName) {
+            leagueNameList.push(leagueName);
+        }
+    }
+    //console.log(leagueNameList);
+
+	for (let player of leagueNameList) {
 		//fetch the playerId's
 		const playerIdResponse = await fetch(
 			`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${player}?api_key=${APIKey}`
@@ -25,7 +33,13 @@ export default async function playerFetcher() {
 
 		//in game
 		if (statusCode == 200) {
-			playersInGame.push(player);
+			for (let x of playerAccountInfo) {
+                for (let y of x.leagueName) {
+                    if (player === y) {
+                        playersInGame.push(x);
+                    }
+                }
+            }
 		}
 	}
 

ebadf9598e7bdd6ca81d7c70c264f6c5322da20b
Gavin <tai.gavin@gmail.com>
2023-09-28 20:19:07 -0400

    blah

--- DiscordBot.js
+++ DiscordBot.js
@@ -1,16 +1,38 @@
 import { Client, GatewayIntentBits } from 'discord.js';
 import keys from './keys.json' assert { type: "json" };
+import playerFetcher from './PlayerIdFetcher.js';
 
-const clientToken = keys.CLIENT_Token;
+const clientToken = keys.clientToken;
 const client = new Client({ intents: [ 
   GatewayIntentBits.DirectMessages,
   GatewayIntentBits.Guilds,
   GatewayIntentBits.GuildMessages,
   GatewayIntentBits.MessageContent,] });
 
+async function getPlayersInGame() {
+  const playersInGame = playerFetcher();
+
+
+}
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  //change time interval to 300000
+  setInterval(async () => {
+    const players = await getPlayersInGame()
+    console.log(players);
+  }, 1000);
+
+  //someone is in game
+
+    //someone in game and not streaming
+
+    //else someone in game and is already streaming
+
+  //else no one is currently in game
 });
+
+
 
 client.on("messageCreate", (message) => {
 if (message.content === "ping") {
--- PlayerIdFetcher.js
+++ PlayerIdFetcher.js
@@ -1,29 +1,36 @@
 import fetch from 'node-fetch';
 import keys from './keys.json' assert { type: "json" };
+import lolPlayers from './playerList.json' assert { type: "json" };
 
-//list of summoner names you want to track (riot limits 20 calls per second)
-const playerList = ["Blowskie", "Who Fed Zed", "Cros92", "Bill Tompkins", "Asmustwar", "H4zed", "H4Z3D"];
-const APIKey = keys.API_Key;
+export default async function playerFetcher() {
+    //list of summoner names you want to track (riot limits 20 calls per second, 100 per 2 minutes)
+    const players = lolPlayers.lolPlayerList;
+    const APIKey = keys.apiKey;
+    const playersInGame = [];
 
-for (let player in playerList) {
-    //fetch the playerId's
-    const playerIdResponse = await fetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${playerList[player]}?api_key=${APIKey}`);
-    const summonerInfo = await playerIdResponse.text();
-    const playerId = JSON.parse(summonerInfo).id;
+    for (let player in players) {
+        //fetch the playerId's
+        const playerIdResponse = await fetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${players[player]}?api_key=${APIKey}`);
+        const summonerInfo = await playerIdResponse.text();
+        const playerId = JSON.parse(summonerInfo).id;
 
-    console.log(`[${playerList[player]}] playerId = [${playerId}]`);
+        //console.log(`[${players[player]}] playerId = [${playerId}]`);
 
-    //fetch response code of player fetch to see if in game or not (404 not in game, 200 in game)
-    const playerStatusResponse = await fetch(`https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${playerId}?api_key=${APIKey}`);
-    const statusCode = playerStatusResponse.status;
+        //fetch response code of player fetch to see if in game or not (404 not in game, 200 in game)
+        const playerStatusResponse = await fetch(`https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${playerId}?api_key=${APIKey}`);
+        const statusCode = playerStatusResponse.status;
 
-    if(statusCode == 400) { //bad request
-        console.log(`Status code for ${playerList[player]} is ${statusCode}. Bad Request.`)
+        if(statusCode == 400) { //bad request
+            //console.log(`Status code for ${players[player]} is ${statusCode}. Bad Request.`)
+        }
+        else if(statusCode == 200) { //in game
+            //console.log(`Status code for ${players[player]} is ${statusCode}. They are currently in game!`)
+            playersInGame.push(players[player]);
+        }
+        else { //not in game
+            //console.log(`Status code for ${players[player]} is ${statusCode}. They are currently not in game.`)
+        }
     }
-    else if(statusCode == 200) { //in game
-        console.log(`Status code for ${playerList[player]} is ${statusCode}. They are currently in game!`)
-    }
-    else { //not in game
-        console.log(`Status code for ${playerList[player]} is ${statusCode}. They are currently not in game.`)
-    }
+
+    return playersInGame;
 }

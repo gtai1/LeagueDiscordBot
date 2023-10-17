a4b8fd17ad1f76f9180deec56448b48007e9e57f
Ryan Lubin <ryan.lubin1@gmail.com>
2023-10-16 21:17:17 -0400

    added await >.>

--- DiscordBot.js
+++ DiscordBot.js
@@ -28,7 +28,7 @@ client.on(Events.ClientReady, async () => {
 	//change time interval to 300000 (5 min)
 
 	const leagueSnitch = new LeagueSnitch(client, serverId, textChannelId);
-	setInterval(leagueSnitch.accusePlayers(), 300000);
+	setInterval(await leagueSnitch.accusePlayers(), 300000);
 });
 
 client.login(clientToken);
